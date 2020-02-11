70a96b936e59aacc975ca93e72c199b1ac1a2112
a.yaacoud <a.yaacoub@ydigitalmedia.com>
2020-02-11 18:34:32 +0000

    Javascript added snake Collision

--- main.js
+++ main.js
@@ -63,3 +63,14 @@ var foodDraw = function() {
         }
     }
 }
+
+var snakeEatHimself = function (x, y, array) {
+    for(var i = 0; i < array.length; i++) {
+        if(array[i].x === x && array[i].y === y)
+        return true;
+    }
+    return false;
+}
+
+
+//Movement
