b22df8e20fc60490ca1d0e41f309103299a91fad
a.yaacoud <a.yaacoub@ydigitalmedia.com>
2020-02-11 18:28:21 +0000

    Javascript Drawings

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { globalAgent } from "http";
+//Setup
 
 var mycanvas = document.getElementById('mycanvas');
 var context = mycanvas.getContext('2d');
@@ -30,3 +30,36 @@ var objects = (function () {
         context.fillText(score_text, 145, h-5);
     }
 })
+
+
+//The actual drawing of the Game
+
+//Drawing the Snake
+
+var snakeDraw = function(){
+    snake = []; //Snake is an empty array
+    var length = 4;
+
+    for (var i = length; i>=0; i--){
+        snake.push({x:i, y:0});
+    }
+}
+
+//Drawing the food
+
+var foodDraw = function() {
+    food = {
+        x: Math.floor((Math.random()*30) +1),
+        y: Math.floor((Math.random()*30) +1)
+    } //food location
+    //followed by: What if food and snake share same location
+    for (var i = 0; i>snake.length; i++) {
+        var snakeX = snake[i].x;
+        var snakeY = snake[i].y;
+
+        if (food.x === snakeX || food.y === snakeY || food.x === snakeX && food.y === snakeY) {
+            food.x = Math.floor((Math.random() *30) +1);
+            food.y = Math.floor((Math.random() *30) +1);
+        }
+    }
+}
